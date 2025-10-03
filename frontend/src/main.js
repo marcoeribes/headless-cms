9316fe643f8050dc523ff7f1fd643293a2171550
Marco <marcoeribes@gmail.com>
2025-10-03 16:51:23 -0700

    Use router to create about and home view

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app')
+import "./assets/main.css";
+import App from './App.vue';
+import router from './router'
+
+const app = createApp(App);
+app.use(router)
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        /*options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }*/
+    }
+ });
+app.mount('#app');
---
+++ frontend/src/pages/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Home</h2>
+  <button @click="goToAbout">Go to About</button>
+  <GoogleCalendar />
+  <h2>Done</h2>
+</template>
+
+<script>
+import GoogleCalendar from '../components/GoogleCalendar.vue';
+
+export default {
+  components: {
+    GoogleCalendar,
+  },
+  methods: {
+    goToAbout() {
+      this.$router.push('/about')
+      },
+  },
+}
+</script>
