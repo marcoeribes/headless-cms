44bf1a30650d5b29cd17cbd00b1cd38cbb4dc9be
Marco <marcoeribes@gmail.com>
2025-10-03 10:22:30 -0700

    calendar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,13 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div style="max-width: 800px; margin: 0 auto;">
+    <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 75%;">
+      <iframe src="https://calendar.google.com/calendar/embed?src=fea36fefd66cf60e363e16dbb4981172875779e12da7881227802bca853b68c1%40group.calendar.google.com&ctz=America%2FPhoenix" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen></iframe>
+    </div>
+  </div>
+
+  <a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NnM4bWZkYW1qOGJjdmlvNDVlaW12bjYzb3MgbWFyY29lcmliZXNAbQ&amp;tmsrc=marcoeribes%40gmail.com"><img border="0" src="https://calendar.google.com/calendar/images/ext/gc_button1_en.gif" alt="Google Calendar"></a>
 </template>
 
 <style scoped></style>
