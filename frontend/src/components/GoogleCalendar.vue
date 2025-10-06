e2b1cc1a978d3a2146a35f19192db25407edb095
Marco <marcoeribes@gmail.com>
2025-10-06 11:13:15 -0700

    Updated Class Vue to add embedded google maps, refactored Calendar vue

--- frontend/src/components/GoogleCalendar.vue
+++ frontend/src/components/GoogleCalendar.vue
@@ -1,7 +1,16 @@
+<script setup>
+  const originalCalendarUrl = "https://calendar.google.com/calendar/embed?src=fea36fefd66cf60e363e16dbb4981172875779e12da7881227802bca853b68c1%40group.calendar.google.com&ctz=America%2FPhoenix";
+  const calendarUrl = "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FPhoenix&showPrint=0&showCalendars=0&title=Schedule&showTitle=0&src=ZmVhMzZmZWZkNjZjZjYwZTM2M2UxNmRiYjQ5ODExNzI4NzU3NzllMTJkYTc4ODEyMjc4MDJiY2E4NTNiNjhjMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23d50000&color=%23009688";
+</script>
+
 <template>
   <div style="max-width: 800px; margin: 0 auto;">
     <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 75%;">
-      <iframe src="https://calendar.google.com/calendar/embed?src=fea36fefd66cf60e363e16dbb4981172875779e12da7881227802bca853b68c1%40group.calendar.google.com&ctz=America%2FPhoenix" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen></iframe>
+      <iframe  
+        :src="calendarUrl"
+        style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" 
+        allowfullscreen>
+      </iframe>
     </div>
   </div>
 </template> 
--- frontend/src/pages/Class.vue
+++ frontend/src/pages/Class.vue
@@ -1,21 +1,92 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { computed } from 'vue'; 
+import { useRoute } from 'vue-router'; 
 import { useStore } from '@/store.js';
 import { storeToRefs } from 'pinia';
+import Button from 'primevue/button';
+import 'primeicons/primeicons.css'
 
 const store = useStore();
-const router = useRouter();
-const { classes } = storeToRefs(store);
+const route = useRoute();
+const { classes, contact } = storeToRefs(store);
+const classId = computed(() => route.params.id);
 
-function goToAbout() {
-  router.push('/about');
-}
+const c = computed(() => {
+  if (classes.value && classId.value) {
+    return classes.value.find(item => item.id === classId.value);
+  }
+  return null; 
+});
+
+const mapUrl = computed(() => {
+  if (c.value?.location) {
+    const encodedLocation = encodeURIComponent(c.value.location);
+    return `https://maps.google.com/maps?q=${encodedLocation}&output=embed`;
+  }
+  return '';
+});
+
 </script>
 
 <template>
-  <h2>Class!</h2>
-  <button @click="goToAbout">Go to About</button>
+  <div Class="page-container">
 
-  
-  <h2>Done</h2>
+    <div v-if="c">
+      <h2>{{ c.title }}</h2>
+      <p>
+        <i class="pi pi-info-circle"></i>
+        <strong>Description:</strong> {{ c.description }}
+      </p>
+      <p>
+        <i class="pi pi-calendar"></i>
+        <strong>Date:</strong> {{ c.month }} {{ c.day }} @ {{ c.startTime }} - {{ c.endTime }}
+      </p>
+      <p>
+        <i class="pi pi-credit-card"></i>
+        <strong>Price:</strong> {{ c.price }}
+      </p>
+      <p>
+        <i class="pi pi-calendar"></i>
+        <strong>Location:</strong> {{ c.location || 'N/A' }}
+      </p>
+      <p>
+        <i class="pi pi-envelope"></i>
+        <strong>Email:</strong> {{ contact.email }}
+      </p>
+      <p>
+        <i class="pi pi-phone"></i>
+        <strong>Call or Text:</strong> {{ contact.phone }}
+      </p>
+
+      <Button 
+        type="button" 
+        label="Register" 
+        size="large" 
+        aria-label="Register" 
+        :loading="loading" 
+        @click="load" 
+      />
+    </div>
+
+    <div v-else-if="classes.length === 0">
+      <p>Loading class data...</p>
+    </div>
+    <div v-else>
+      <p>Class with ID **{{ classId }}** not found.</p>
+    </div>
+
+    <div v-if="c?.location" style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe
+        :src="mapUrl"
+        style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .page-container i {
+    margin-right: 0.5rem;
+  }
+</style>
