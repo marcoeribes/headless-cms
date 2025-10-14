61da12cfdfd344a2caf0ab03f34def5824f9bfa1
Marco <marcoeribes@gmail.com>
2025-10-14 11:55:06 -0700

    Working post api to sheets

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,22 @@
 import dotenv from "dotenv";
 import * as ga from "./googleApi/google-api.js";
 import Fastify from "fastify";
-import cors from '@fastify/cors'
-import { addTimeProperties } from "./service.js";
+import cors from "@fastify/cors";
+import { addTimeProperties, idGenerator } from "./service.js";
 
 dotenv.config();
 
 const GOOGLE_CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
 
-
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 await fastify.register(cors, {
-  origin: 'http://localhost:5173'
-})
+  origin: "http://localhost:5173",
+});
 
-fastify.get('/api/getEvents', async (req, res) => {
+fastify.get("/api/getEvents", async (req, res) => {
   try {
     const result = await ga.getSheetEvents();
     const formattedData = addTimeProperties(result);
@@ -25,27 +24,32 @@ fastify.get('/api/getEvents', async (req, res) => {
     return { success: true, data: result };
   } catch (error) {
     req.log.error(error);
-    res.code(500).send({ success: false, message: 'Failed to fetch sheets data' });
+    res
+      .code(500)
+      .send({ success: false, message: "Failed to fetch sheets data" });
   }
 });
 
-fastify.get('/api/getCalendarId', async (req, res) => {
+fastify.post("/api/postRegistration", async (req, res) => {
   try {
-    console.log("Calendar ID:", GOOGLE_CALENDAR_ID);
-    return { success: true, data: GOOGLE_CALENDAR_ID};
+    const formattedData = idGenerator(req.body);
+    const result = await ga.saveToSheet(formattedData);
+    console.log("request body: ", req.body);
+    return { success: true, data: result };
   } catch (error) {
     req.log.error(error);
-    res.code(500).send({ success: false, message: 'Failed to fetch calendar Id' });
+    res
+      .code(500)
+      .send({ success: false, message: "Failed to post data to sheets" });
   }
 });
-
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
+};
 start();
--- backend/service.js
+++ backend/service.js
@@ -1,31 +1,43 @@
+// Helper functions for API
 
 export function addTimeProperties(eventsArray) {
-    eventsArray.forEach((event) => {
-        
-        const startDate = new Date(event.startDate);
-        const startTime = startDate.toLocaleTimeString('en-US', {
-            hour: 'numeric', // Use '2-digit' for "08" or 'numeric' for "8"
-            minute: '2-digit',
-            hour12: true 
-        });
-
-        const endDate = new Date(event.endDate);
-        const endTime = endDate.toLocaleTimeString('en-US', {
-            hour: 'numeric',
-            minute: '2-digit',
-            hour12: true
-        });
-
-        const day = startDate.toLocaleDateString('en-US', { day: 'numeric' });
-        const month = startDate.toLocaleDateString('en-US', { month: 'long' });
-        const year = startDate.toLocaleDateString('en-US', { year: 'numeric' });
-
-        event.startTime = startTime;
-        event.endTime = endTime;
-        event.day = day;
-        event.month = month;
-        event.year = year;
+  eventsArray.forEach((event) => {
+    const startDate = new Date(event.startDate);
+    const startTime = startDate.toLocaleTimeString("en-US", {
+      hour: "numeric", // Use '2-digit' for "08" or 'numeric' for "8"
+      minute: "2-digit",
+      hour12: true,
     });
 
-    return eventsArray
+    const endDate = new Date(event.endDate);
+    const endTime = endDate.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    const day = startDate.toLocaleDateString("en-US", { day: "numeric" });
+    const month = startDate.toLocaleDateString("en-US", { month: "long" });
+    const year = startDate.toLocaleDateString("en-US", { year: "numeric" });
+
+    event.startTime = startTime;
+    event.endTime = endTime;
+    event.day = day;
+    event.month = month;
+    event.year = year;
+  });
+
+  return eventsArray;
+}
+
+export function idGenerator(registration) {
+  let result = "";
+  const chars = "0123456789abcdef";
+  for (let i = 0; i < 4; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return {
+    id: result,
+    ...registration,
+  };
 }
